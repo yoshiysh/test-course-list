0ace830f054d91e9042dbec66b6a1828984ab85e
Yoshiki Henmi <yoshiki.henmi@gmail.com>
2019-10-26 04:07:38 +0900

    Add navigation and update main activity layout.

--- app/src/main/kotlin/com/yoshi991/testcourselist/presentation/MainActivity.kt
+++ app/src/main/kotlin/com/yoshi991/testcourselist/presentation/MainActivity.kt
@@ -1,13 +1,31 @@
 package com.yoshi991.testcourselist.presentation
 
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.yoshi991.testcourselist.R
+import com.yoshi991.testcourselist.databinding.ActivityMainBinding
 import dagger.android.support.DaggerAppCompatActivity
 
 class MainActivity : DaggerAppCompatActivity() {
 
+    private val navController: NavController by lazy {
+        findNavController(R.id.main_nav_host)
+    }
+
+    private val binding: ActivityMainBinding by lazy {
+        DataBindingUtil.setContentView<ActivityMainBinding>(
+            this,
+            R.layout.activity_main
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.appbar.setupWithNavController(navController)
     }
 }
